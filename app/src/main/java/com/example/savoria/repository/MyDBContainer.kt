fa5ac16e2784950a84d39250715876eb807c975d
Karyna <karyna.sanjaya@gmail.com>
2024-01-05 20:17:02 +0700

    Login Success

--- app/src/main/java/com/example/savoria/repository/MyDBContainer.kt
+++ app/src/main/java/com/example/savoria/repository/MyDBContainer.kt
@@ -31,7 +31,7 @@ class MyDBContainer() {
         }
     }
 
-    private val BASE_URL = "http://127.0.0.1:8000/"
+    private val BASE_URL = "http://10.0.2.2:8000/api/"
 
 //    private val client = OkHttpClient.Builder()
 //        .addInterceptor(AuthInterceptor(ACCESS_TOKEN))
